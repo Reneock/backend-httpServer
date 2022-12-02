638199f80b3e80b70a7758285cfea89ed42ff82a
Irene Ockrah <ostrene33@gmail.com>
2022-12-01 19:30:42 -0800

    practicing backend

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const http = require('http');
 
 const handleAllRequests = (requestObject, responseObject) => {
   console.log("Hello, a request just came through");
-  const url = requestObject.url;
 
   //console.log(requestObject);
   //html can be written in the responses below
@@ -10,6 +9,7 @@ const handleAllRequests = (requestObject, responseObject) => {
   // responseObject.write("A follow up response on the above request");
 
   //allocating specific urls with their respective handles
+  const url = requestObject.url;
    if (url === '/') responseObject.write ("<h1>This is the home Page</h1>");
    if (url === '/login') responseObject.write ("<h1>Welcome back, login now</h1>");
    if (url === '/signup') responseObject.write ("<h1>Join us now, signup</h1>");
