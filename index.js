8356a6b7a4141c91b776144376d19cda6db8f4b4
Irene Ockrah <ostrene33@gmail.com>
2022-12-15 17:40:04 -0800

    introducing middleware

--- index.js
+++ index.js
@@ -7,10 +7,41 @@ const handleLoginRequest = (req, res) => {res.send("Welcome back, login now");}
 const handleProfileRequest = (req, res) => {res.send("Create your profile");}
 const handleHomeRequest = (req, res) => {res.send("<h1>This is the home Page</h1>");}
 
-//handling server methods
-server.post('/general', handleEveryRequest);
-server.get('/login', handleLoginRequest);
-server.put('/profile', handleProfileRequest);
-server.use('/', handleHomeRequest);
+//introducing middleware
+const middlewareFunction = (req, res, next) => {
+  console.log ("introducing middleware");
+  //checking for mistakes
+  next();
+}
+
+//handling specific middleware
+const generalRouteSpecificMiddleware = (req, res, next) => {
+  console.log ("general route middleware executed");
+  next();
+}
+
+const loginRouteSpecificMiddleware = (req, res, next) => {
+  console.log ("login route middleware executed");
+  next();
+}
+
+const profileRouteSpecificMiddleware = (req, res, next) => {
+  console.log ("profile route middleware executed");
+  next();
+}
+
+const homeRouteSpecificMiddleware = (req, res, next) => {
+  console.log ("home route middleware executed");
+  next();
+}
+
+//generalmiddleware
+server.use(middlewareFunction);
+
+//handling server methods(routes)
+server.post('/general',generalRouteSpecificMiddleware, handleEveryRequest);
+server.get('/login', loginRouteSpecificMiddleware, handleLoginRequest);
+server.put('/profile',profileRouteSpecificMiddleware, handleProfileRequest);
+server.use('/',homeRouteSpecificMiddleware, handleHomeRequest);
 
 server.listen(3000, () => {console.log("Request received, wait for response")});
