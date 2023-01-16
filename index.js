1fe45bdc157085cdb4e3a70a7f9655ded5197f21
Irene Ockrah <ostrene33@gmail.com>
2023-01-15 21:30:07 -0800

    introducing body-parser

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const server = express('server');
 
+const loginRequestHandler = (req, res) => {
+  //console.log(req);
+  //below is manual method of body-parser
+  // let body = "";
+  // req.on('data', chunk => { body += chunk; });
+
+  // req.on('end', ()=>{ 
+  //   //parse with regExp
+  //   console.log(body); 
+  // });
+
+  console.log(req.body.email);
+  console.log(req.body.password);
+  res.send('Done');
+}
 //middleware definitions
 server.use(express.static(path.join(__dirname, 'public')));
+server.use(bodyParser.urlencoded({extended: false}))
+
+//route 
+server.post('/login', loginRequestHandler);
 // const serveHomePage = (req, res) =>{
 //   //res.send('Welcome to the home Page');
 //   //finding the file
